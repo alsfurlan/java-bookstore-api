9405adda180e4460e5aba093860b1be3e3281c6d
Anderson Luis Furlan <alsfurlan@gmail.com>
2021-05-19 21:20:53 -0300

    Continuação do CRUD de autores: GET e POST

--- src/main/java/br/com/bookstore/api/autor/AutorResource.java
+++ src/main/java/br/com/bookstore/api/autor/AutorResource.java
@@ -1,13 +1,72 @@
 package br.com.bookstore.api.autor;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.WebApplicationException;
+import static javax.ws.rs.core.Response.Status;
 
 /**
  *
  * @author aula
  */
-@Path("/autores")
+@Path("autores")
 public class AutorResource {
-    
-    
+
+    static List<Autor> autores = new ArrayList<>(
+        Arrays.asList(
+            new Autor(1, "David Cockford", LocalDate.of(1959, Month.MARCH, 1), Genero.MASCULINO),
+            new Autor(2, "JK Rowling", LocalDate.of(1953, Month.MARCH, 1), Genero.FEMININO)
+        )
+    );
+
+    public AutorResource() {
+
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Autor> getAutores() {
+        return autores;
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Autor getAutor(@PathParam("id") int id) {
+        Autor autor = autores
+                .stream()
+                .filter(a -> id == a.getId())
+                .findFirst()
+                .orElse(null);
+
+        if (autor == null) {
+            throw new WebApplicationException("Autor não encontrado", Status.NOT_FOUND);
+        }
+
+        return autor;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Autor addAutor(Autor autor) {
+        int ultimoId = autores.get(autores.size()-1).getId();
+        autor.setId(++ultimoId);
+        autores.add(autor);
+        return autor;
+    }
+
 }
